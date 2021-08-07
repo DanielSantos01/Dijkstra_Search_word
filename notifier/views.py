a9f0eaaeeb09d7d1d980ea4f051709c5bbb32339
Daniel Santos <danielspovv@gmail.com>
2021-08-07 17:42:49 -0300

    test: short curcuit

--- notifier/views.py
+++ notifier/views.py
@@ -5,5 +5,5 @@ class HandleGetWayViewSet(viewsets.ViewSet):
   # POST
   def create(self, request):
     # message, status_code = self.__handle_body(request.POST.dict())
-    return Response('haha', status=200)
+    return Response(request.body, status=200)
 
