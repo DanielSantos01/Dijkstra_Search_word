249694854508aedf01d58b2e8bcbb08089e7070b
Mauricio Sightman <mauriciosightman@gmail.com>
2021-08-07 19:13:57 -0300

    algoritimo funcionando

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from data_handler import get_data
-from node import Node
+from node import Node, List
+
 
 def main():
-  data = get_data()
-  nodes = []
-  for node in data['nodes']:
-    nodes.append(Node(node['id'], node['value'], node['label']))
+    data = get_data()
+    nodes = []
+    for node in data['nodes']:
+        nodes.append(Node(node['id'], node['value'], node['label']))
 
-  for edge in data['edges']:
-    source = nodes[edge['source']]
-    target = nodes[edge['target']]
-    if source.value == 1 and target.value == 0:
-      target.link_to(source)
-    else:
-      source.link_to(target)
+    for edge in data['edges']:
+        source = nodes[edge['source']]
+        target = nodes[edge['target']]
+        source.link_to(target)
+
+    graph = List(nodes)
+    print(graph.find_connection(0, 15))
+
 
 if __name__ == '__main__':
-  main()
+    main()
--- node.py
+++ node.py
@@ -1,16 +1,94 @@
 class Node:
-  def __init__(self, id, value, label):
-    self.id = id
-    self.value = value
-    self.label = label
-    self.visited = False
-    self.links = []
-  
-  def visit(self):
-    self.visited = True
+    def __init__(self, id, value, label):
+        self.id = id
+        self.value = value
+        self.label = label
+        self.visited = False
+        self.links = []
 
-  def unvisit(self):
-    self.visited = False
+    def visit(self):
+        self.visited = True
 
-  def link_to(self, node):
-    self.links.append(node)
+    def unvisit(self):
+        self.visited = False
+
+    def link_to(self, node):
+        self.links.append(node)
+
+
+class List:
+    def __init__(self, nodes):
+        length = len(nodes)
+        self.list = nodes
+        self.vertex_distance = [0] * length
+        self.before = [0] * length
+
+    def __merge(self, vetor, left, middle, right):
+        i = left
+        j = middle + 1
+
+        aux = vetor[::]
+
+        for index in range(left, right + 1):
+            if i > middle:
+                vetor[index] = aux[j]
+                j += 1
+            elif j > right:
+                vetor[index] = aux[i]
+                i += 1
+            elif aux[i]["value"] > aux[j]["value"]:
+                vetor[index] = aux[j]
+                j += 1
+            else:
+                vetor[index] = aux[i]
+                i += 1
+
+    def __top_down(self, vetor, left, right):
+        if left >= right:
+            return
+        middle = (left + right) // 2
+
+        self.__top_down(vetor, left, middle)
+        self.__top_down(vetor, middle + 1, right)
+
+        self.__merge(vetor, left, middle, right)
+
+    def __merge_sort(self, list):
+        self.__top_down(list, 0, len(list) - 1)
+
+    def __relax(self, source, target, weight):
+        if self.vertex_distance[target] > self.vertex_distance[source] + weight:
+            self.before[target] = source
+            self.vertex_distance[target] = self.vertex_distance[source] + weight
+
+    def run_dijikstra(self, id):
+        ordened_list = []
+        resp = []
+
+        for v in self.list:
+            self.vertex_distance[v.id] = 9999999
+            self.before[v.id] = -1
+
+        self.vertex_distance[id] = 0
+        for v in self.list:
+            ordened_list.append({"value": self.vertex_distance[v.id], "id": v.id})
+            
+        while len(ordened_list) > 0:
+            self.__merge_sort(ordened_list)
+            current = ordened_list[0]
+            ordened_list = ordened_list[1:]
+            resp.append(current)
+            
+            for vertex in self.list[current["id"]].links:
+                self.__relax(current["id"], vertex.id, 1)
+
+    def find_connection(self, source, target):
+        self.run_dijikstra(source)
+        resp = [target]
+
+        last_vertex = self.before[target]
+        while last_vertex != -1:
+            resp.append(last_vertex)
+            last_vertex = self.before[last_vertex]
+
+        return resp
